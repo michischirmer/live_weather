9573b5c38c775288a272d19cf3b13369fc931c50
michischirmer <msschirm20@gmail.com>
2021-03-07 23:10:28 +0100

    Added raspi files

---
+++ data_fetcher.py
@@ -0,0 +1,15 @@
+from cs50 import SQL
+import bme280
+from datetime import datetime
+import time
+
+db = SQL("sqlite:///data.db")
+while(1):
+	print("Reading Value & Adding to DB")
+	temperature,pressure,humidity = bme280.readBME280All()
+
+	now = datetime.now()
+	timestamp = datetime.timestamp(now)
+
+	db.execute("INSERT INTO datapoints ([Temperature], [Humidity], [Pressure], [Timestamp]) VALUES (?, ?, ?, ?);", temperature, pressure, humidity, timestamp)
+	time.sleep(10)
