d6968e8bc827acb474f7c1833fcf370163c4df87
michischirmer <64130395+michischirmer@users.noreply.github.com>
2021-03-08 21:56:30 +0100

    changed database for avg

--- sql/average.sql
+++ sql/average.sql
@@ -1,2 +1,7 @@
 SELECT round(avg(temperature), 2) as avg_Temperature, round(avg(pressure), 2) as avg_Pressure, round(avg(humidity), 2) as avg_Humidity
-FROM datapoints
+FROM (
+	SELECT round(temperature, 3) as Temperature, round(humidity, 2) as Humidity, round(pressure, 2) as Pressure, timestamp
+	FROM datapoints 
+	ORDER BY Timestamp DESC 
+	LIMIT 40
+)
