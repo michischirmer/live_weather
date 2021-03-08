9d484dc20e312d38f002f4aafa9d0eaedad65151
michischirmer <64130395+michischirmer@users.noreply.github.com>
2021-03-08 15:59:09 +0100

    added selection to none

--- static/js/compare.js
+++ static/js/compare.js
@@ -1,11 +1,21 @@
 var time = [], temperature = [], pressure = [], humidity = [];
 var count = 0;
 
-var temperature_data = [0, 24, window.chartColors.yellow];
-var pressure_data = [940, 1030, window.chartColors.red];
-var humidity_data = [0, 100, window.chartColors.blue];
+var temperature_data = [0, 24, window.chartColors.yellow, temperature, 'Temperature'];
+var pressure_data = [940, 1030, window.chartColors.red, pressure, 'Air Pressure'];
+var humidity_data = [0, 100, window.chartColors.blue, humidity, 'Humidity'];
+var sets = [temperature_data, pressure_data, humidity_data];
 
 var current_left = temperature_data, current_right = pressure_data;
+var left_display = true, right_display = true;
+
+function init(){
+    temperature_data = [0, 24, window.chartColors.yellow, temperature, 'Temperature'];
+    pressure_data = [940, 1030, window.chartColors.red, pressure, 'Air Pressure'];
+    humidity_data = [0, 100, window.chartColors.blue, humidity, 'Humidity'];
+    sets = [temperature_data, pressure_data, humidity_data];
+}
+
 
 function get_data(){    
     var json;
@@ -23,39 +33,44 @@ function get_data(){
     for(let i = 1; i < time.length; i += 2){
         time[i] = "";
     }
-    
     setTimeout(start, 100);
 
     function start() {
         time = [], temperature = [], pressure = [], humidity = [];
+        temperature_data = [], pressure_data = [], humidity_data = [];
         json['data'].forEach(element => {
             time.push(element['Time']);
             temperature.push(element['Temperature']);
 			pressure.push(element['Pressure']);
-			humidity.push(element['Humdity']);
+			humidity.push(element['Humidity']);
         });
     }
+    temperature_data = [0, 24, window.chartColors.yellow, temperature, 'Temperature'];
+    pressure_data = [940, 1030, window.chartColors.red, pressure, 'Air Pressure'];
+    humidity_data = [0, 100, window.chartColors.blue, humidity, 'Humidity'];
+    sets = [temperature_data, pressure_data, humidity_data];
 }
 
 function update(){
     get_data();
+    init();
     var ctx = document.getElementById('canvas').getContext('2d');
     window.myLine = Chart.Line(ctx, {
         data: {
 			labels: time,
 			datasets: [{
-				label: 'Temperature',
+				label: current_left[4],
 				borderColor: current_left[2],
 				backgroundColor: current_left[2],
 				fill: false,
-				data: temperature,
+				data: current_left[3],
 				yAxisID: 'y-axis-1',
 			}, {
-				label: 'Pressure',
+				label: current_right[4],
 				borderColor: current_right[2],
 				backgroundColor: current_right[2],
 				fill: false,
-				data: pressure,
+				data: current_right[3],
 				yAxisID: 'y-axis-2'
 			}]
 		},
@@ -104,24 +119,25 @@ function update(){
 }
 
 window.onload = function() {
+    current_left = temperature_data, current_right = pressure_data;
     Chart.defaults.global.animation.duration = 0;
     var ctx = document.getElementById('canvas').getContext('2d');
     window.myLine = Chart.Line(ctx, {
         data: {
 			labels: time,
 			datasets: [{
-				label: 'Temperature',
+				label: current_left[4],
 				borderColor: current_left[2],
 				backgroundColor: current_left[2],
 				fill: false,
-				data: temperature,
+				data: current_left[3],
 				yAxisID: 'y-axis-1',
 			}, {
-				label: 'Pressure',
+				label: current_right[4],
 				borderColor: current_right[2],
 				backgroundColor: current_right[2],
 				fill: false,
-				data: pressure,
+				data: current_right[3],
 				yAxisID: 'y-axis-2'
 			}]
 		},
@@ -164,3 +180,29 @@ window.onload = function() {
     setTimeout(get_data, 100);
     setTimeout(update, 100);
 };
+
+function changed(val, direction){
+    if (direction == 0){
+        if (val == 3){
+            left_display = false;
+            current_left = [0, 10, window.chartColors.grey, [0,0,0,0,0], '']
+        }else{
+            left_display = true;
+            current_left = sets[val];
+        }   
+    }else if (direction == 1){
+        if (val == 3){
+            right_display = false;
+            current_right = [0, 10, window.chartColors.grey, [0,0,0,0,0], '']
+        }else{
+            right_display = true;
+            current_right = sets[val];
+        }  
+    }
+    temperature_data = [0, 24, window.chartColors.yellow, temperature, 'Temperature'];
+    pressure_data = [940, 1030, window.chartColors.red, pressure, 'Air Pressure'];
+    humidity_data = [0, 100, window.chartColors.blue, humidity, 'Humidity'];
+    sets = [temperature_data, pressure_data, humidity_data];
+    console.log(sets[val]);
+    update();
+}
